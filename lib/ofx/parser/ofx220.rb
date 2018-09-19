59c3fce115c0f60ae6930a80dc6616566d7209ae
Paul Parker <paulstuartparker@gmail.com>
2018-09-19 13:40:27 -0400

    Spec Account Discovery/2.2

--- lib/ofx/parser/ofx220.rb
+++ lib/ofx/parser/ofx220.rb
@@ -13,7 +13,7 @@ module OFX
         nested_account_info = node.search('bankacctfrom, ccacctfrom')
         OFX::AccountInfo.new({
           name:              node.search('name').inner_text,
-          description:       node.search('desc').inner_text,
+          description:       node.search('desc').inner_text.upcase,
           bank_id:           nested_account_info.search('bankid').inner_text,
           id:                nested_account_info.search('acctid').inner_text,
           type:              nested_account_info.search('accttype').inner_text.upcase
